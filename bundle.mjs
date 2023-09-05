6aea9836200a4cef3286d67fe08e53b1d30a1298
Haydn Comley <haydn.comley@clearscore.com>
2023-09-05 18:10:12 +0100

    Auto Docs

---
+++ bundle.mjs
@@ -0,0 +1,13 @@
+import * as esbuild from 'esbuild';
+import fs from 'fs';
+
+fs.rmSync('./docs', { recursive: true, force: true });
+fs.cpSync('./example', './docs', { recursive: true, force: true });
+fs.rmSync('./docs/assets/js', { recursive: true, force: true });
+
+await esbuild.build({
+    entryPoints: ['example/assets/js/index.js'],
+    bundle: true,
+    minify: true,
+    outfile: 'docs/assets/js/index.js'
+})
